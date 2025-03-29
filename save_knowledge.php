c42e1af604247a324501bc98e2d5b5473a5828ae
lindian-zhao <tz_tongzhao@foxmail.com>
2025-03-30 00:57:02 +0800

    Update save_knowledge.php

--- save_knowledge.php
+++ save_knowledge.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
